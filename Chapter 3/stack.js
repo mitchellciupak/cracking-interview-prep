08da788c96406d2b9fc5fbe28b6970d969ef3441
mitchellciupak <ciupak@purdue.edu>
2020-12-17 12:49:10 -0500

    3.0

---
+++ Chapter 3/stack.js
@@ -0,0 +1,41 @@
+// 2.1 Linked List Remove Dups/Remove Duplicates
+// Mitchell Ciupak
+// 20201217
+
+/*
+Implament a stack
+Page: (96,107)
+*/
+
+class Stack {
+    constructor() {
+        this.stack = []
+    }
+
+    pop() {
+        if (this.stack.length == 0) {
+            return;
+        }
+
+        let temp = this.stack[this.stack.length - 1];
+
+        this.stack.splice(this.stack.length - 1, 1)
+
+        return temp;
+    }
+
+    push(item) {
+
+        this.stack.push(item);
+        return true;
+
+    }
+
+    peek() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    isEmpty() {
+        return this.peek() == null;
+    }
+}
