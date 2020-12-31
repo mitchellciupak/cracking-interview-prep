fd6c4473f90907a6d069579d23c4a3c2cd27d2d9
mitchellciupak <ciupak@purdue.edu>
2020-12-31 10:18:28 -0600

    3.3

--- Chapter 3/stackofPlates.js
+++ Chapter 3/stackofPlates.js
@@ -12,3 +12,47 @@ SetOfStacks. push() and SetOfStacks. pop() should behave identically to a single
 
 Page: (99,110)
 */
+
+class SetOfStacks {
+    constructor(singleStackCapacity, totalCapacity) {
+        this.cap = totalCapacity;
+        n = totalCapacity / singleStackCapacity;
+        this.SetOfStacks = new Array(n);
+        for (let i = 0; i < n; ++i) a[i] = new Array(singleStackCapacity).fill(0);
+        this.whichStack = 0;
+    }
+
+    pop() {
+        if (this.SetOfStacks[0].length == 0) {
+            return;
+        }
+
+        if (this.SetOfStacks[this.whichStack][this.stack.length] == this.cap) {
+            this.whichStack -= 1;
+        }
+
+        let temp = this.SetOfStacks[this.whichStack][this.stack.length - 1];
+
+        this.stack.splice(this.SetOfStacks[this.whichStack].length - 1, 1)
+
+        return temp;
+    }
+
+    push(item) {
+
+        if (this.SetOfStacks[this.whichStack][this.stack.length] == this.cap) {
+            this.whichStack += 1;
+        }
+        this.SetOfStacks[this.whichStack].push(item);
+        return true;
+
+    }
+
+    peek() {
+        return this.stack[this.SetOfStacks[this.whichStack].length - 1];
+    }
+
+    isEmpty() {
+        return this.peek() == null;
+    }
+}
