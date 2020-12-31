a64650a7730523a3a49eece4904b2a20f9394474
mitchellciupak <ciupak@purdue.edu>
2020-12-31 08:43:55 -0600

    3.1-3.3 Files Created

---
+++ Chapter 3/stackMin.js
@@ -0,0 +1,9 @@
+// 3.2 - Stack Min
+// Mitchell Ciupak
+// 20201231
+
+/*
+How would you design a stack which, in addition to push and pop, has a function min
+which returns the minimum element? Push, pop and min should all operate in 0(1) time.
+Page: (98,109)
+*/
---
+++ Chapter 3/threeinOne.js
@@ -0,0 +1,8 @@
+// 3.1 - Three in one
+// Mitchell Ciupak
+// 20201231
+
+/*
+Describe how you could use a single array to implement three stacks.
+Page: (98,109)
+*/
