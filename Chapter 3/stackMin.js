d2aa85fd50d5dce788a30b013a2599d446bb5c78
mitchellciupak <ciupak@purdue.edu>
2020-12-31 10:07:36 -0600

    3.2

--- Chapter 3/stackMin.js
+++ Chapter 3/stackMin.js
@@ -7,3 +7,50 @@ How would you design a stack which, in addition to push and pop, has a function 
 which returns the minimum element? Push, pop and min should all operate in 0(1) time.
 Page: (98,109)
 */
+
+class Stack {
+    constructor() {
+        this.stack = [];
+        this.min = 0;
+        this.lastmin = 0;
+    }
+
+    pop() {
+        if (this.stack.length == 0) {
+            return;
+        }
+
+        let temp = this.stack[this.stack.length - 1];
+
+        if (temp == this.min) {
+            this.min = this.lastmin;
+        }
+
+        this.stack.splice(this.stack.length - 1, 1);
+
+        return temp;
+    }
+
+    push(item) {
+
+        if (this.stack.length == 0 || item < this.min) {
+            this.lastmin = this.min;
+            this.min = item;
+        }
+        this.stack.push(item);
+        return true;
+
+    }
+
+    peek() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    isEmpty() {
+        return this.peek() == null;
+    }
+
+    min() {
+        return this.min;
+    }
+}
