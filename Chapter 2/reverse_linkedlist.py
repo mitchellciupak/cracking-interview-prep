c20be995e09e022307665724f04af9f091231e15
mitchellciupak <ciupak@purdue.edu>
2021-03-03 09:10:37 -0500

    2.ReverseLinkedList

---
+++ Chapter 2/reverse_linkedlist.py
@@ -0,0 +1,32 @@
+# Reverse Linked List
+## Mitchell Ciupak
+## 20210303
+
+import linkedLists as ll
+
+def reverse_linkedlist(head):
+    prev = None
+    curr = head
+    nxt = head.next
+
+    while curr is not None:
+        nxt = curr.next # temp storage
+
+        curr.next = prev# assign next to last (essential for reversal)
+        prev = curr     # update prev
+        curr = nxt      # increment curr
+
+    head = prev
+    return head
+
+
+
+# Create List to Reverse
+head = ll.Node(1)
+head.next = ll.Node(2)
+head.next.next = ll.Node(3)
+head.next.next.next = ll.Node(4)
+head.next.next.next.next = ll.Node(5)
+
+head = reverse_linkedlist(head=head)
+ll.printLL(head)
