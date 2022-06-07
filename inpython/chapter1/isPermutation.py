f6bc19027dfffd788ccb46f2d8ba0c1b5c59ce45
mitchellciupak <ciupak@purdue.edu>
2022-06-07 10:22:54 -0500

    py: ispermutation

--- inpython/chapter1/isPermutation.py
+++ inpython/chapter1/isPermutation.py
@@ -1,64 +1,43 @@
-// 1.2 Arrays and Stirngs - Check Permutations
-// Mitchell Ciupak
-// 20201104
+# 1.2 Arrays and Stirngs - Check Permutations
+# Mitchell Ciupak
+# 20201104
 
-/*
+"""
 Check Permutation: Given two strings, write a method to decide if one is a permutation of the
 other.
 Page(90,101)
 
 Note: Permutation - is true of parent sring has the same number of character counts as child
-*/
+"""
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
+def isPermutation(parent: str, child: str):
+    # permutations are of the same length
+    if len(parent) != len(child):
+        return False
 
-bool isPermutation(char* parent, char* child) {
+    # create a hashmap of the alphabet (only has to be a list because we can dirive the correct index from ascii conversion)
+    map = [0] * 26
 
-    //Check lengths of string
-    unsigned int plen = strlen(parent);
-    unsigned int clen = strlen(child);
+    # populate parent with occurances of chars in string into hashmap
+    for letter in parent.lower():
+        letter_as_index = ord(letter) - 97
+        map[letter_as_index] += 1
 
-    if (plen != clen) { return false; }
+    # compare child to parent with map lookup
+    for letter in child.lower():
+        letter_as_index = ord(letter) - 97
+        map[letter_as_index] -= 1
+    
+        if map[letter_as_index] < 0:
+            return False
+    
+    return True
 
-    //Create hash map (assuming only 26 lowercase letters)
-    int* letters = calloc(sizeof(int), 26);
+if __name__ == "__main__":
+    parent = "isth"
+    child = "this"
 
-    //Populate hash map w/ parent occurances
-    for (int i = 0; i < plen; i++) {
-        letters[(int)parent[i] - 48]++;
-    }
-
-    //Check hash map w/ child
-    for (i = 0; i < clen; i++)
-    {
-        unsigned int index = (int)child[i] - 48;
-
-        letters[index]--;
-
-        if (letters[index] < 0)
-        {
-            return false;
-        }
-    }
-
-    free(letters);
-    return true;
-}
-
-void main()
-{
-
-    char parent[] = "this is the parent";
-    char child[] = "this";
-
-    bool ret = isUnique(parent, child);
-
-    if (ret) {
-        printf("%c is a permutation of %c.", child, parent);
-    }
-    else {
-        printf("%c is not a permutation of %c.", child, parent););
-    }
-}
+    if isPermutation(parent,child):
+        print(child + " is a permutation of " + parent)
+    else:
+        print(child + " is not a permutation of " + parent)
