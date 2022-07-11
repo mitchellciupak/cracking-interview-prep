5e47d7e5bdd7ac664b699bf316485bfa293cfa55
mitchellciupak <mjcre23@gmail.com>
2022-07-11 09:30:09 -0400

    go/intro

---
+++ ingo/chapter1/go.mod
@@ -0,0 +1,21 @@
+module example
+
+go 1.18
+
+// go mod init example
+// go mod tidy
+
+
+require rsc.io/quote v1.5.2
+
+require (
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
+
+
+// https://go.dev/doc/
+// [x] https://go.dev/doc/tutorial/getting-started
+// [ ] https://go.dev/doc/tutorial/create-module
+// [ ] https://go.dev/doc/code
+// [ ] https://go.dev/tour/welcome/1
--- ingo/chapter1/isPermutation.go
+++ ingo/chapter1/isPermutation.go
@@ -1,32 +1,12 @@
 // integers.go
 package main
 
-// Multiply returns the product of two integers
-func Multiply(a, b int) int {
-	return a * b
-}
+import (
+	"fmt"
 
-func isPermutation(parent, child) {
-    // permutations are of the same length
-    if (parent.length != child.length) {return False;}
+	"rsc.io/quote"
+)
 
-    // create a hashmap of the alphabet (only has to be a list because we can dirive the correct index from ascii conversion)
-    map = new Array(len).fill(26);
-
-    // populate parent with occurances of chars in string into hashmap
-    for (let letter in parent.toLowerCase()) {
-        letter_as_index = letter.charCodeAt(0) - 97
-        map[letter_as_index] += 1
-    }
-
-    // compare child to parent with map lookup
-    for (let letter in child.toLowerCase()) {
-        letter_as_index = letter.charCodeAt(0) - 97
-        map[letter_as_index] -= 1
-    
-        if (map[letter_as_index] < 0 ){ return False;}
-    }
-
-    return True
-
+func main() {
+	fmt.Println(quote.Go())
 }
