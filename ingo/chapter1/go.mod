61016911fe6e85e18e2f33cbd2a646169745e7c1
mitchellciupak <mjcre23@gmail.com>
2022-07-12 16:49:10 -0400

    go/intro2

--- ingo/chapter1/go.mod
+++ ingo/chapter1/go.mod
@@ -12,10 +12,3 @@ require (
 	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
 	rsc.io/sampler v1.3.0 // indirect
 )
-
-
-// https://go.dev/doc/
-// [x] https://go.dev/doc/tutorial/getting-started
-// [ ] https://go.dev/doc/tutorial/create-module
-// [ ] https://go.dev/doc/code
-// [ ] https://go.dev/tour/welcome/1
--- ingo/chapter1/isPermutation.go
+++ ingo/chapter1/isPermutation.go
@@ -10,3 +10,30 @@ import (
 func main() {
 	fmt.Println(quote.Go())
 }
+
+// testing 
+package morestrings
+
+import "testing"
+
+func TestReverseRunes(t *testing.T) {
+    cases := []struct {
+        in, want string
+    }{
+        {"Hello, world", "dlrow ,olleH"},
+        {"Hello, 世界", "界世 ,olleH"},
+        {"", ""},
+    }
+    for _, c := range cases {
+        got := ReverseRunes(c.in)
+        if got != c.want {
+            t.Errorf("ReverseRunes(%q) == %q, want %q", c.in, got, c.want)
+        }
+    }
+}
+
+// https://go.dev/doc/
+// [x] https://go.dev/doc/tutorial/getting-started
+// [x] https://go.dev/doc/tutorial/create-module
+// [x] https://go.dev/doc/code
+// [ ] https://go.dev/tour/welcome/1
